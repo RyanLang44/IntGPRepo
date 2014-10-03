7a9f45d1779cdec454def5031ee126baf3d19555
Weescottishkid <ryanlang44@gmail.com>
2014-10-03 10:54:26 +0100

    hello world

---
+++ New Unity Project/Assets/testscript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World")
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
